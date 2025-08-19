a85be7d4a9337d0e221e031ddf536281e993f434
İpek Öner <109289547+ipranek@users.noreply.github.com>
2025-08-19 17:03:13 +0300

    Add files via upload

---
+++ data/boycott_data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+data = {
+    "Brand" : ["Espressolab", "D&R", "Watsons", "Mavi", "Kırmızı Kedi"],
+    "Boycott_Status" : ["Boycotted", "Boycotted", "Boycotted", "Not Boycotted", "Not Boycotted"],
+    "Reason" : ["..."]
+}
