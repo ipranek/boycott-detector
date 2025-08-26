7c81a9eda068ff8740c7c00d31d788f59c6f622a
İpek Öner <ipekoner2006@gmail.com>
2025-08-26 13:34:59 +0300

    cors

--- app.py
+++ app.py
@@ -2,14 +2,15 @@
 from flask import Flask, render_template, jsonify
 import sqlite3
 import pandas as pd
-import CORS 
+import flask_cors as CORS
 
 app = Flask(__name__)
-
+CORS(app, resources={r"/*": {"origins": "*"}}, methods=["GET"]) 
 @app .route("/")
 def home():
     return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port = 5001)
+
 
