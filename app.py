9791d0a3e4553a35346cb0f4bfc44b157cf94033
İpek Öner <ipekoner2006@gmail.com>
2025-08-26 14:09:42 +0300

    Update app.py
    updated the db connection

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ import pandas as pd
 import flask_cors as CORS
 
 app = Flask(__name__)
+
+DB_PATH = "database/boycott_info.db"
+
+def get_db_connection():
+    conn = sqlite3.connect("database/boycott_info.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
 CORS(app, resources={r"/*": {"origins": "*"}}, methods=["GET"]) 
 @app .route("/")
 def home():
