2eea24c0266a3eb0098ba09b2fae159562aa1b49
İpek Öner <ipekoner2006@gmail.com>
2025-08-26 14:05:09 +0300

    sql database

--- database/create_database.py
+++ database/create_database.py
@@ -5,7 +5,7 @@ import os
 
 boycott_info = pd.read_csv("/Users/ipekoner/Documents/GitHub/boycott-detector/data/boycott_reasons.csv")
 
-conn = sqlite3.connect("boycott_info.pd")
+conn = sqlite3.connect("boycott_info.db")
 
 cursor = conn.cursor()
 
@@ -14,7 +14,7 @@ cursor.execute("""
             Brand TEXT PRIMARY KEY,
             Boycott_Status TEXT,
             Reason TEXT,
-            Citation NOT NULL
+            Citation TEXT NOT NULL
             );
 """)
 
@@ -25,3 +25,5 @@ boycott_info.to_sql("boycott_info", conn, if_exists="append", index=False)
 
 conn.commit()
 conn.close()
+
+print("CSV successfully saved to SQLite database!")
